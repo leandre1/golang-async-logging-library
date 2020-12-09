405ace7449d21b01f19633d8713d973bb4581faa
Lbiza <lbizagwira@gmail.com>
2020-12-09 14:53:38 +0000

    changes2

--- alog.go
+++ alog.go
@@ -100,6 +100,7 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
 	al.shutdownCompleteCh <- struct{}{}
 	<-al.shutdownCompleteCh
 }
